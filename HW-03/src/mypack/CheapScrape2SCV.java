7ab3a989b7f85691f69aaf5dcc160abbbfd72e43
cnguyen1110 <cnguyen1110@OVDIMC10-374V.coast.cccd.adx>
2019-09-25 12:44:05 -0700

    added homework to github

---
+++ HW-03/src/mypack/CheapScrape2SCV.java
@@ -0,0 +1,44 @@
+package mypack;
+import java.util.*;
+import java.io.*;
+
+public class CheapScrape2SCV
+{
+
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        // TODO Auto-generated method stub
+        for(int i = 0; i < args.length; i++) 
+        {
+            System.out.println(args[i]);
+        }
+        
+        // Process the command line args kinda like this:
+        
+        int columns = Integer.parseInt(args[0]);
+        String inputFile = args[1];
+        String outputFile = args[2];
+        
+       
+        Scanner in = new Scanner(new File(inputFile));
+        PrintWriter out = new PrintWriter(outputFile);
+        while(in.hasNextLine())
+        {
+            for(int i = 0; i < columns; i++)
+            {
+                String hold = in.nextLine();
+                if(i == 1)
+                {
+                    out.print("\"" + hold + "\"");
+                }
+                //out.print(hold);
+                
+            }
+            
+        }
+        
+        
+
+    }
+
+}
